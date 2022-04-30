7c8bfcceb5987ab4b41bfd6b3dfea8653d7c51bf
Matias <maturod96@gmail.com>
2022-04-29 21:56:28 -0300

    Changes the Access-Control-Allow-Headers because it was wrong before.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-      'Access-Allow-Control-Headers', 
+      'Access-Control-Allow-Headers', 
       'Origin, X-Requested-With, content-type, Accept, Z-Key');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
